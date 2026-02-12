ff99d4a7ac7af677567eaf894cf69a578c49d2a3
Bert P. <bert.plonus@t-online.de>
2026-02-12 10:26:06 +0100

    Add .gitignore and clean up dependency tracking

--- backend/dist/monitor/monitor.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { MonitorLog } from './entities/monitor-log.entity';
-export declare class MonitorController {
-    private readonly logRepository;
-    constructor(logRepository: Repository<MonitorLog>);
-    getLogs(limit?: number): Promise<MonitorLog[]>;
-    getStats(): Promise<any[]>;
-}
--- backend/dist/monitor/openapi-parser/openapi-parser.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class OpenapiParserService {
-    private readonly logger;
-    parseDefinition(filePath: string): Promise<any>;
-    extractEndpoints(api: any): any[];
-}
